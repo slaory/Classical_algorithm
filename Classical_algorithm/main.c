55f67b4067006ad2dde4a1aa970d3d1b72e612cc
slaory <slaory@qq.com>
2019-08-02 22:22:21 +0800

    Initial Commit

---
+++ Classical_algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Classical_algorithm
+//
+//  Created by fanhaifei on 2019/8/2.
+//  Copyright © 2019 fanhaifei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
